b7dedffbb883f25f657b3bd263470d2588692db8
mvanderhulst <m.van.der.hulst@st.hanze.nl>
2026-04-07 10:10:46 +0200

    Eerste opzet class DemoClassifier

--- model.py
+++ model.py
@@ -2,6 +2,33 @@ import pandas as pd
 import numpy as np
 
 class DemoClassifier:
-    def predict(self, filename="competition_test.csv"):
-        data = pd.read_csv(filename)
+    """verhaal lalal"""
+
+    def __init__(self, filename="competition_test.csv"):
+        """
+        sllal
+        """
+        self.filename = filename
+
+        return 0
+
+    def __repr__(self):
+        """
+        string representation for debugging purposes
+        :return: string
+        """
+
+        return f"stringverhaallala{self.filename}"
+
+    def dataprep(self):
+        """
+        dataprep dingen
+        :return:
+        """
+        data = pd.read_csv(self.filename, sep=",", header=0)
+        return data
+
+
+    def predict(self):
+        data = self.dataprep()
         return np.random.choice([True, False], size=(data.shape[0],))
