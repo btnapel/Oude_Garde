3d91517928d3143abc86723e169e448be6eb8083
mvanderhulst <m.van.der.hulst@st.hanze.nl>
2026-04-07 11:22:11 +0200

    update Democlassifier(dataprep)

--- model.py
+++ model.py
@@ -1,16 +1,24 @@
+"""
+model om voorspellingen te doen
+
+
+Author: Bert ten Napel, Maartje van der Hulst
+Date: 7-4-2026
+"""
+
+
 import pandas as pd
 import numpy as np
 
 class DemoClassifier:
     """verhaal lalal"""
 
-    def __init__(self, filename="competition_test.csv"):
+    def __init__(self, filename="data-studenten.csv"):
         """
         sllal
         """
         self.filename = filename
-
-        return 0
+        self.data = self.dataprep()
 
     def __repr__(self):
         """
@@ -25,10 +33,19 @@ class DemoClassifier:
         dataprep dingen
         :return:
         """
-        data = pd.read_csv(self.filename, sep=",", header=0)
+        data = pd.read_csv(self.filename, sep=",", header=0, na_values="?",
+                           true_values=["+"], false_values=["-"])
+        for i in ("hypertensie", "hartinfarct", "diabetes", "nierziekte"):
+            data[i].replace({"+", 1}, {"-", 0}, inplace=True)
+        # print(data["prognose10jaar"].head())
         return data
 
 
     def predict(self):
         data = self.dataprep()
         return np.random.choice([True, False], size=(data.shape[0],))
+
+
+
+DemoClassifier()
+
